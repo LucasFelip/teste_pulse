50fef8f4437357bd450e6397007e4c696d1572ed
Lucas Ferreira <luscafreis@gmail.com>
2025-05-06 21:06:13 -0300

    initial commit

---
+++ src/main/java/com/solohub/teste_pulse/TestePulseApplication.java
@@ -0,0 +1,13 @@
+package com.solohub.teste_pulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestePulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestePulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solohub/teste_pulse/TestePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solohub.teste_pulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestePulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

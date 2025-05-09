66fbb6ff6772c0bf9d6481ff0ea014871f0f35a3
Lucas Ferreira <luscafreis@gmail.com>
2025-05-09 09:33:11 -0300

    fix: adiciona em models ItemPedidoModel

---
+++ src/main/java/com/solohub/teste_pulse/api/model/ItemPedidoModel.java
@@ -0,0 +1,20 @@
+package com.solohub.teste_pulse.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = true,onlyExplicitlyIncluded = true)
+public class ItemPedidoModel extends AbstractModel {
+    private Long produtoId;
+    private Integer quantidade;
+    private BigDecimal precoUnitario;
+}

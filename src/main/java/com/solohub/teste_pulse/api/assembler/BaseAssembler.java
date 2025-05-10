26163f96ef46cad990fd698bcac427667c6cefd2
Lucas Ferreira <luscafreis@gmail.com>
2025-05-10 01:06:38 -0300

    fix: retorno de elemento nulo

--- src/main/java/com/solohub/teste_pulse/api/assembler/BaseAssembler.java
+++ src/main/java/com/solohub/teste_pulse/api/assembler/BaseAssembler.java
@@ -5,35 +5,28 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public abstract class BaseAssembler<E, D extends RepresentationModel<?>, C>
         extends RepresentationModelAssemblerSupport<E, D> {
 
     protected final ModelMapper mapper;
-    protected final Class<C> controllerClass;
 
     protected BaseAssembler(ModelMapper mapper,
                             Class<C> controllerClass,
                             Class<D> dtoClass) {
         super(controllerClass, dtoClass);
         this.mapper = mapper;
-        this.controllerClass = controllerClass;
     }
 
     @Override
     public D toModel(E entity) {
-        return mapper.map(entity, getResourceType());
+        D dto = instantiateModel(entity);
+        System.out.println("entity: " + entity);
+        mapper.map(entity, dto);
+        return dto;
     }
 
     @Override
     public CollectionModel<D> toCollectionModel(Iterable<? extends E> entities) {
-        List<D> dtos =
-                ((List<E>) entities).stream()
-                        .map(this::toModel)
-                        .collect(Collectors.toList());
-        return CollectionModel.of(dtos);
+        return super.toCollectionModel(entities);
     }
 }
-
